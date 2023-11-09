f583673d41fa4d674ccf2654f01bb6d88366bdb8
mcristhian <75554603+mcristhian@users.noreply.github.com>
2023-11-09 09:29:55 -0300

    Altera a criação do banco

--- banco.sql
+++ banco.sql
@@ -1,48 +1,57 @@
 create database urna;
 
-create table administrador (
-	id serial primary key,
+create table administrador(
+	id_administrador serial primary key,
+  nome text not null,
+  email text not null unique,
+  senha text not null unique
+);
+
+create table eleicao(
+	id_eleicao serial primary key,
+  id_administrador int references administrador(id_administrador),
+  nome text not null,
+  votos int,
+  cadeiras int not null
+);
+
+create table partido(
+	id_partido serial primary key,
+  id_eleicao int references eleicao(id_eleicao),
+  id_lider int,
   nome text not null,
   email text not null unique,
   senha text not null
 );
 
-create table eleicao (
-	id serial primary key,
-  id_administrador int references administrador(id),
-	nome text not null,
-  votos int,
-  cadeiras int not null,
-  candidatos int 
-);
-
-create table partido (
-	id serial primary key,
-  id_eleicao int references administrador(id),
-  id_lider int,
-  nome text not null,
-  email text not null unique,
-  senha text not null,
-  votos int
-);
-
-create table deputado (
-	id serial primary key,
-  id_partido int references partido(id),
+create table deputado(
+	id_deputado serial primary key,
+  id_partido int references partido(id_partido),
   nome text not null,
   eleito boolean
 );
 
-create table eleitor (
-	id serial primary key,
-  id_eleicao int references eleicao(id),
+create table eleitor(
+	id_eleitor serial primary key,
+  id_eleicao int references eleicao(id_eleicao),
   nome text not null,
   email text not null unique,
   senha text not null,
   votou boolean
 );
 
+create table resultado(
+	id_resultado serial primary key,
+  id_eleicao int references eleicao(id_eleicao),
+  id_partido int references partido(id_partido),
+  candidatos int,
+  votos int,
+  porcentagem_votos numeric(3,2),
+  cadeiras int, 
+  porcentagem_cadeiras numeric(3,2)
+);
+
 alter table partido
-	add constraint fk_deputado
-  foreign key (id_lider)
-  references deputado(id);
+add constraint fk_deputado
+foreign key (id_lider)
+references deputado (id_deputado);
