ebdaa6a9bad1d652692b516f7dd0012cae10e0e8
mcristhian <75554603+mcristhian@users.noreply.github.com>
2023-12-07 07:12:35 -0300

    Aplica correções na validação do esquema de cadastro do partido

---
+++ src/validacoes/esquemaCadastroDeputado.js
@@ -0,0 +1,9 @@
+const joi = require('joi')
+
+const esquemaCadastroDeputado = joi.object({
+    nome: joi.string().required().messages({
+        'any.required': 'O campo nome é obrigatório.',
+        'string.empty': 'O campo nome é obrigatório.',
+        'string.base': 'O campo '
+    })
+})
--- src/validacoes/esquemaCadastroPartido.js
+++ src/validacoes/esquemaCadastroPartido.js
@@ -7,27 +7,21 @@ const esquemaCadastroPartido = joi.object({
     'number.base': 'O campo id_eleicao só aceita números.',
     'number.integer': 'O campo id_eleicao só aceita números inteiros.'
   }),
-//   id_lider: joi.number().integer().required().messages({
-//     'any.required': 'O campo id_lider é obrigatório.',
-//     'number.empty': 'O campo id_lider é obrigatório.',
-//     'number.base': 'O campo id_lider só aceita números.',
-//     'number.integer': 'O campo id_lider só aceita números inteiros.'
-//   }),
   nome: joi.string().required().messages({
     'any.required': 'O campo nome é obrigatório.',
     'string.empty': 'O campo nome é obrigatório.',
-    'string.base': 'O campo cadeiras só aceita palavras.'
+    'string.base': 'O campo nome só aceita palavras.'
   }),
   email: joi.string().email().required().messages({
     'any.required': 'O campo email é obrigatório.',
     'string.empty': 'O campo email é obrigatório.',
-    'string.email': 'O campo email precisar ter um formato válido.',
-    'string.base': 'O campo cadeiras só aceita palavras.'
+    'string.email': 'O campo email precisa ter um formato válido.',
+    'string.base': 'O campo email só aceita palavras.'
   }),
   senha: joi.string().required().messages({
     'any.required': 'O campo senha é obrigatório.',
     'string.empty': 'O campo senha é obrigatório.',
-    'string.base': 'O campo cadeiras só aceita palavras.'
+    'string.base': 'O campo senha só aceita palavras.'
   })
 })
 
