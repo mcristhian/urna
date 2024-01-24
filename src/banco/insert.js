cf820a2fa9e0acf553234fbc1e59261b917fdce2
Matheus <75554603+mcristhian@users.noreply.github.com>
2024-01-24 14:48:08 -0300

    Insere o resultado individual no momento do cadastro do partido

--- src/banco/insert.js
+++ src/banco/insert.js
@@ -14,6 +14,7 @@ const cadastrarEleicaoQuery = async (id_administrador, nome, cadeiras) => {
 const cadastrarPartidoQuery = async (id_eleicao, nome, email, posicao_economica, posicao_social, senha) => {
     return pool('partido')
     .insert({ id_eleicao, nome, email, posicao_economica, posicao_social, senha })
+    .returning('*')
 }
 
 const cadastrarDeputadoQuery = async (id_partido, nome, lider) => {
@@ -26,10 +27,16 @@ const cadastrarEleitorQuery = async (id_eleicao, nome, email, senha, votou) => {
     .insert({ id_eleicao, nome, email, senha, votou })
 }
 
+const cadastrarResultadoDoPartidoQuery = async (id_eleicao, id_partido, candidatos, votos, porcentagem_votos, cadeiras, porcentagem_cadeiras) => {
+    return pool('resultado')
+    .insert({ id_eleicao, id_partido, candidatos, votos, porcentagem_votos, cadeiras, porcentagem_cadeiras })
+}
+
 module.exports = {
     cadastrarAdministradorQuery,
     cadastrarEleicaoQuery,
     cadastrarPartidoQuery,
     cadastrarDeputadoQuery,
-    cadastrarEleitorQuery
+    cadastrarEleitorQuery,
+    cadastrarResultadoDoPartidoQuery
 }
