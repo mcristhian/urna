96ef5467cc474df3ec966bd203b376b1e7156b0a
Matheus <75554603+mcristhian@users.noreply.github.com>
2024-01-24 16:05:40 -0300

    Implementa a função para registro do voto

---
+++ src/controladores/resultado.js
@@ -0,0 +1,36 @@
+const { listarEleicaoPorIdQueryAlternativa, listarPartidoPorIdEEleicaoQuery } = require("../banco/select")
+const { registrarVotoQuery } = require("../banco/update")
+
+const votar = async (req, res) => {
+    const { senha: _, ...eleitor } = req.eleitor
+    const { id: voto } = req.params
+
+    try {
+        const eleicao = await listarEleicaoPorIdQueryAlternativa(eleitor.id_eleicao)
+
+        if (!eleicao) {
+            return res.status(404).json({ mensagem: 'Eleição não encontrada.' })
+        }
+
+        if (eleitor.votou === true) {
+            return res.status(400).json({ mensagem: 'Eleitor já registrou seu voto.' })
+        }
+
+        const partido = await listarPartidoPorIdEEleicaoQuery(voto, eleitor.id_eleicao)
+
+        if (!partido) {
+            return res.status(404).json({ mensagem: 'Partido não encontrado.' })
+        }
+
+        await registrarVotoQuery(eleitor.id_eleitor)
+
+        return res.status(200).json(partido)
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
+    }
+}
+
+module.exports = {
+    votar
+}
