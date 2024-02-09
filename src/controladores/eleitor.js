e9dc307e236f18d38cd670d0d0628c4867f5af68
Matheus <75554603+mcristhian@users.noreply.github.com>
2024-02-09 09:30:41 -0300

    Verifica status da eleição antes do cadastro

--- src/controladores/eleitor.js
+++ src/controladores/eleitor.js
@@ -9,6 +9,12 @@ const cadastrarEleitor = async (req, res) => {
     const { id_eleicao, nome, email, senha, votou } = req.body
 
     try {
+        const { finalizada: eleicaoFinalizada } = await listarEleicaoPorIdQueryAlternativa(id_eleicao)
+
+        if (eleicaoFinalizada) {
+            return res.status(400).json({ mensagem: 'Eleição finalizada.' })
+        }
+
         const eleitorComMesmoEmail = await listarEleitorPorEmailQuery(email)
 
         if (eleitorComMesmoEmail) {
