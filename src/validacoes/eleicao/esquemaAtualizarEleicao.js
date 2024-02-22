57a94cbd75b0a1e9b593f8e88c74a73e2a108d19
Matheus <75554603+mcristhian@users.noreply.github.com>
2024-02-22 11:17:08 -0300

    Impede o cadastro e atualização para cadeiras ímpares

--- src/validacoes/eleicao/esquemaAtualizarEleicao.js
+++ src/validacoes/eleicao/esquemaAtualizarEleicao.js
@@ -5,10 +5,11 @@ const esquemaAtualizarEleicao = joi.object({
         'string.empty': 'O campo nome não pode estar vazio.',
         'string.base': 'O campo nome só aceita palavras.'
     }),
-    cadeiras: joi.number().integer().messages({
+    cadeiras: joi.number().integer().multiple(2).messages({
         'number.empty': 'O campo cadeiras não pode estar vazio.',
         'number.base': 'O campo cadeiras só aceita números.',
-        'number.integer': 'O campo cadeiras só aceita números inteiros.'
+        'number.integer': 'O campo cadeiras só aceita números inteiros.',
+        'number.multiple': 'O campo cadeiras só aceita números pares.'
     }),
     finalizada: joi.boolean().messages({
         'boolean.empty': 'O campo finalizada não pode estar vazio.',
--- src/validacoes/eleicao/esquemaCadastroEleicao.js
+++ src/validacoes/eleicao/esquemaCadastroEleicao.js
@@ -5,11 +5,12 @@ const esquemaCadastroEleicao = joi.object({
         'any.required': 'O campo nome é obrigatório.',
         'string.empty': 'O campo nome é obrigatório.'
     }),
-    cadeiras: joi.number().integer().required().messages({
+    cadeiras: joi.number().integer().multiple(2).required().messages({
         'any.required': 'O campo cadeiras é obrigatório.',
         'number.empty': 'O campo cadeiras é obrigatório.',
         'number.base': 'O campo cadeiras só aceita números.',
-        'number.integer': 'O campo cadeiras só aceita números inteiros.'
+        'number.integer': 'O campo cadeiras só aceita números inteiros.',
+        'number.multiple': 'O cmapo cadeira só aceita números pares.'
     })
 })
 
