84b719cfcea727629014671e9f50b9c3fdc207b9
Matheus <75554603+mcristhian@users.noreply.github.com>
2024-02-23 08:09:13 -0300

    Retira estrutura trycatch

--- src/controladores/resultado.js
+++ src/controladores/resultado.js
@@ -13,14 +13,14 @@ const votar = async (req, res) => {
             return res.status(404).json({ mensagem: 'Eleição não encontrada.' })
         }
 
-        let eleicaoFinalizada = eleicao.finalizada
-        if (eleicaoFinalizada) {
-            return res.status(400).json({ mensagem: 'Eleição finalizada.' })
-        }
+        // let eleicaoFinalizada = eleicao.finalizada
+        // if (eleicaoFinalizada) {
+        //     return res.status(400).json({ mensagem: 'Eleição finalizada.' })
+        // }
 
-        if (eleitor.votou === true) {
-            return res.status(400).json({ mensagem: 'Eleitor já registrou seu voto.' })
-        }
+        // if (eleitor.votou === true) {
+        //     return res.status(400).json({ mensagem: 'Eleitor já registrou seu voto.' })
+        // }
 
         const partido = await listarPartidoPorIdEEleicaoQuery(voto, id_eleicao)
 
@@ -46,32 +46,29 @@ const votar = async (req, res) => {
 
         return res.status(200).json({ mensagem: 'Voto contabilizado.' })
     } catch (error) {
+        console.log(error.message)
         return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
     }
 }
 
 const atualizarResultadoDoPartido = async (id_eleicao) => {
-    try {
-        const resultados = await listarResultadoPorEleicaoQuery(id_eleicao)
-        const { cadeiras } = await listarEleicaoPorIdQueryAlternativa(id_eleicao)
+    const resultados = await listarResultadoPorEleicaoQuery(id_eleicao)
+    const { cadeiras } = await listarEleicaoPorIdQueryAlternativa(id_eleicao)
 
-        for (resultado of resultados) {
-            const { votos: votosDoPartido } = await listarResultadoPorPartidoQuery(resultado.id_partido)
-            
-            const { votos: votosTotais } = await listarEleicaoPorIdQueryAlternativa(id_eleicao)
+    for (resultado of resultados) {
+        const { votos: votosDoPartido } = await listarResultadoPorPartidoQuery(resultado.id_partido)
+        
+        const { votos: votosTotais } = await listarEleicaoPorIdQueryAlternativa(id_eleicao)
 
-            const porcentagem_votos = votosDoPartido / votosTotais
-            let cadeirasDoPartido = cadeiras * porcentagem_votos
-            cadeirasDoPartido = Math.round(cadeirasDoPartido)
-            await distribuirCadeirasQuery(cadeirasDoPartido, resultado.id_partido)
-            //Consertar distribuição de vagas
-            await atualizarPorcentagemDeVotosQuery(porcentagem_votos * 100, resultado.id_partido)
+        const porcentagem_votos = votosDoPartido / votosTotais
+        let cadeirasDoPartido = cadeiras * porcentagem_votos
+        cadeirasDoPartido = Math.round(cadeirasDoPartido)
+        await distribuirCadeirasQuery(cadeirasDoPartido, resultado.id_partido)
+        //Consertar distribuição de vagas
+        await atualizarPorcentagemDeVotosQuery(porcentagem_votos * 100, resultado.id_partido)
 
-            let porcentagemCadeiras = cadeirasDoPartido / cadeiras
-            await atualizarPorcentagemDeCadeirasQuery(porcentagemCadeiras * 100, resultado.id_partido)
-        }
-    } catch (error) {
-        return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
+        let porcentagemCadeiras = cadeirasDoPartido / cadeiras
+        await atualizarPorcentagemDeCadeirasQuery(porcentagemCadeiras * 100, resultado.id_partido)
     }
 }
 
