807dae852f994babb886a432e2832c1834faa2b3
Matheus <75554603+mcristhian@users.noreply.github.com>
2024-02-26 20:30:17 -0300

    Retira a limitação de assentos

--- src/validacoes/eleicao/esquemaCadastroEleicao.js
+++ src/validacoes/eleicao/esquemaCadastroEleicao.js
@@ -5,7 +5,7 @@ const esquemaCadastroEleicao = joi.object({
         'any.required': 'O campo nome é obrigatório.',
         'string.empty': 'O campo nome é obrigatório.'
     }),
-    cadeiras: joi.number().integer().multiple(2).required().messages({
+    cadeiras: joi.number().integer().required().messages({
         'any.required': 'O campo cadeiras é obrigatório.',
         'number.empty': 'O campo cadeiras é obrigatório.',
         'number.base': 'O campo cadeiras só aceita números.',
